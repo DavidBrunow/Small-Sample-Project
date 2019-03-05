b17c632f521c6081e9e6c53646d9ed9c019f7ba1
David Brunow <david@shep.dog>
2019-03-05 08:04:05 -0600

    Initial commit

---
+++ Small Sample Project/Small_Sample_Project.h
@@ -0,0 +1,19 @@
+//
+//  Small_Sample_Project.h
+//  Small Sample Project
+//
+//  Created by David Brunow on 3/5/19.
+//  Copyright © 2019 Shepherd Dog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Small_Sample_Project.
+FOUNDATION_EXPORT double Small_Sample_ProjectVersionNumber;
+
+//! Project version string for Small_Sample_Project.
+FOUNDATION_EXPORT const unsigned char Small_Sample_ProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Small_Sample_Project/PublicHeader.h>
+
+
